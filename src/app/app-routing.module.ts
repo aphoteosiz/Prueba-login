a831fe03484e1cefe41979afdc25a72972b4eaba
aphoteosiz <mm3262348@gmail.com>
2023-10-06 18:02:08 -0700

    tercer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { RecuperacionComponent } from './auth/recuperacion/recuperacion.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/inicio', pathMatch: 'full' },
   { path: 'inicio', component: DashboardComponent },
   { path: 'iniciar-sesion', component: LoginComponent },
-  {path: 'login', component: LoginComponent}
+  {path: 'login', component: LoginComponent},
+  { path: 'recuperacion', component:RecuperacionComponent}
 ];
 
 @NgModule({
